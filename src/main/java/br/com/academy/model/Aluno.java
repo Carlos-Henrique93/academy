507e28d4956b31373d30fbd81f1b906ec148d6ea
Carlos Henrique <carlos.henriquesantos@hotmail.com>
2024-11-04 22:59:51 -0300

    Enviando dados do formulario para mysql

--- src/main/java/br/com/academy/model/Aluno.java
+++ src/main/java/br/com/academy/model/Aluno.java
@@ -2,74 +2,76 @@ package br.com.academy.model;
 
 import br.com.academy.Enums.Curso;
 import br.com.academy.Enums.Status;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
+import jakarta.persistence.*;
 
 @Entity
 public class Aluno {
-	
-	@Id 
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer id;
-	
-	@Column (name = "nome")
-	private String nome;
-	
-	@Column (name ="curso")
-	private Curso curso;
-	
-	@Column (name ="matricula")
-	private String matricula;
-	
-	@Column (name = "status")
-	private Status status;
-	
-	@Column (name = "turno")
-	private String turno;
-	
-	
-	public Integer getId() {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String nome;
+    
+    @Enumerated(EnumType.STRING)
+    private Curso curso;
+    
+    public Long getId() {
 		return id;
 	}
-	public void setId(Integer id) {
+
+	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public Curso getCurso() {
 		return curso;
 	}
+
 	public void setCurso(Curso curso) {
 		this.curso = curso;
 	}
+
 	public String getMatricula() {
 		return matricula;
 	}
+
 	public void setMatricula(String matricula) {
 		this.matricula = matricula;
 	}
-	
-	public String getTurno() {
-		return turno;
-	}
-	public void setTurno(String turno) {
-		this.turno = turno;
-	}
+
 	public Status getStatus() {
 		return status;
 	}
+
 	public void setStatus(Status status) {
 		this.status = status;
 	}
 
-	
+	public String getTurno() {
+		return turno;
+	}
+
+	public void setTurno(String turno) {
+		this.turno = turno;
+	}
+
+	private String matricula;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+    
+    @Column(nullable = false)
+    private String turno;
+
+    // Getters e setters...
 }
